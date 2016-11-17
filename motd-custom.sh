a6eece77396d3db44f7cba3fe47a80203a60b52a
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2016-11-17 09:31:49 +0100

    Update motd-custom.sh

--- motd-custom.sh
+++ motd-custom.sh
@@ -30,14 +30,15 @@ curRam="$(echo "$memUsed/$memTot*100" | bc -l | awk -F'.' '{print $1 "%"}')";
 curCpu="$(echo "100 - $cpuUsed" | bc -l)";
 curHome="$(df /home | awk '{print $5}' | tail -n 1)";
 
-col1="\e[0;0m";
-col2="\e[0;31m";
-col3="\e[0;33m";
-col4="\e[1;33m";
+col1="\e[0;0m";  # white
+col2="\e[0;31m"; # red
+col3="\e[0;33m"; # dark yellow
+col4="\e[1;33m"; # yellow
 
 
-echo -en "$col1   System.......$col2=$col3 "; /bin/uname -snrvm | awk -F" " '{print $4}';
-echo -en "$col1   Release......$col2=$col3 "; cat /etc/*release | grep DESC | cut -d'"' -f2;
+#echo -en "$col1   System.......$col2=$col3 "; /bin/uname -snrvm | awk -F" " '{print $4}';
+echo -en "$col1   System.......$col2=$col3 "; uname -v;
+echo -en "$col1   Release......$col2=$col3 "; cat /etc/*release | grep PRETTY | cut -d'"' -f2;
 echo -en "$col1   Kernel.......$col2=$col3 "; uname -rs;
 echo -en "$col1   Uptime.......$col2=$col3 "; awk '{print int($1/86400)" day "int($1%86400/3600)":"int(($1%3600)/60)":"int($1%60)}' /proc/uptime;
 echo -en "$col1   Load.........$col2=$col3 "; 
@@ -50,4 +51,3 @@ echo -en "$col1   Process......$col2=$col3 "; top -bn1 | awk '/Tasks/ {print $2 
 echo -en "$col1   IP...........$col2=$col3 "; ip route get 8.8.8.8 | head -1 | cut -d' ' -f8;
 
 echo -e "\033[0m";
-
