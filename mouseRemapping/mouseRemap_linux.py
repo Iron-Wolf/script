c857d51afc8973b7ccd5259fa54620b44005fe0d
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2021-03-15 18:28:00 +0100

    Add files via upload

---
+++ mouseRemapping/mouseRemap_linux.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3 
+# -*- coding: utf-8 -*- 
+
+"""
+doc...
+
+Dependencies
+mouse : https://github.com/boppreh/mouse
+"""
+
+import mouse
+import subprocess
+import re #regex
+
+MOUSE_ID='12'
+MOUSE_MAPPING='1 2 3 4 5 6 7 0 0'
+AUTO_SCROLL_STEP=10 # time before auto scroll
+AUTO_SCROLL_INTERVAL=30 # control auto scroll speed (milliseconds)
+
+
+"""
+Get the actual physical state
+of the given button
+"""
+def butonState(button):
+  # for python < 3.7
+  #result = subprocess.run(["xinput", "--query-state", "12"], stdout=subprocess.PIPE, universal_newlines=True)
+  # for python >= 3.7
+  #result = subprocess.run(["xinput", "--query-state", "12"], capture_output=True, universal_newlines=True)
+  #print("r:",result.stdout)
+
+  # captured output is returned as bytes
+  # use "universal_newlines=True" ("text=True" for python >= 3.7)
+  # to capture as text
+  result = subprocess.check_output(["xinput", "--query-state", MOUSE_ID], 
+    universal_newlines=True)
+  
+  regex = re.search('button\[{0}\]=down'.format(button), result)
+  if regex:
+    #print("regex:", regex.group(0))
+    return True
+  else:
+    return False
+
+"""
+
+"""
+def scrollFunction(wheelDir, buttonNum):
+  #mouse.wheel(-1) # <- don't work because desactivated ?
+  subprocess.run(["xdotool", "click", wheelDir])
+
+  # Loop used to "pause" the script before auto scoll.
+  # We check if the button is actually held down (to
+  # determine if this is a TRUE held down gesture all along)
+  for i in range(AUTO_SCROLL_STEP):
+    state=butonState(buttonNum)
+    if state:
+      pass
+    else:
+      return
+  
+  keepLoop=butonState(buttonNum)
+  while keepLoop:
+    # set "repeat" flag to control "delay"
+    subprocess.run(["xdotool", "click", 
+      "--repeat", "1", 
+      "--delay", str(AUTO_SCROLL_INTERVAL),
+      wheelDir])
+    keepLoop=butonState(buttonNum)
+
+
+# +------+
+# | MAIN |
+# +------+
+def main(): 
+  # remove all registered events, just to be sure
+  mouse.unhook_all()
+
+  # Disable default mouse behaviour and
+  # control all from this script.
+  # Use "shell=True" to pass a single string because
+  # the "run" command escape the arguments.
+  subprocess.run(["xinput set-button-map {0} {1}".format(MOUSE_ID, MOUSE_MAPPING)], shell=True)
+  subprocess.run(["xinput", "--get-button-map", MOUSE_ID])
+
+  mouse.on_button(scrollFunction,("5","8"),'x','down')
+  mouse.on_button(scrollFunction,("4","9"),'x2','down')
+
+
+# enable use of the script as an imported module
+if __name__ == "__main__": 
+	main()
+
+input('wait...\n')
+
+
+
+
+
+
---
+++ mouseRemapping/mouseRemap_linux.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+# http://www.remycorthesy.fr/montpellier/symboles-codes-caracteres-ascii-iso.htm
+#
+# Basic explanation : https://askubuntu.com/a/492745
+# get device ID :   xinput
+# list all button : xinput list <ID>
+# see key press :   xinput test <ID>
+# get mapping :     xinput --get-button-map <ID>
+# get mouse ID :    xinput --list | grep -i -m 1 'mouse' | grep -o 'id=[0-9]\+' | grep -o '[0-9]\+'
+#
+# +--------------+
+# | DEPENDENCIES |
+# +--------------+
+# xdotool
+#
+#
+# Sample code with windows :
+# eval window under mouse cursor (set WINDOW variable)
+#eval $(xdotool getmouselocation --shell)
+# send keystroke to that window
+#xdotool key --window $WINDOW Up
+
+# disable default mouse behaviour and
+# control all from this script
+mouseId='12'
+#mouseMapping='1 2 3 4 5 6 7 5 4' # side-forward(9) <> wheel-up(4) / side-back <> wheel-down
+mouseMapping='1 2 3 4 5 6 7 0 0' # disable side button
+#mouseMapping='1 2 3 4 5 6 7 8 9' # default map
+xinput set-button-map $mouseId $mouseMapping
+
+# +------+
+# | MAIN |
+# +------+
+autoScrollStep=10 # time before auto scroll
+autoScrollInterval=30 # use it to control auto scroll speed (milliseconds)
+
+# convert to milliseconds (doesn't used)
+#autoScrollInterval=`echo "scale=2; $autoScrollInterval/1000" | bc -l`
+
+
+# infinite loop
+while true; do
+  # wait mouse click to lower CPU usage, 
+  # but doesn't go beyond 0.1s interval
+  # -g : exit on change
+  # -n : interval
+  # -p : precise mode (maybe useless)
+  #watch -g -n 0.1 -p 'xinput --query-state 12 | grep "button\[[89]\]=down"'
+  
+  # +-------------+
+  # | FIRST CLICK |
+  # +-------------+
+  startAutoScroll=false
+  
+  sideBack=`xinput --query-state $mouseId | grep "button\[8\]=down"`
+  # test if variable is set
+  if [ "$sideBack" ]; then
+    # button is held down
+    xdotool click 5 # down
+    startAutoScroll=true
+  fi
+  sideFront=`xinput --query-state $mouseId | grep "button\[9\]=down"`
+  # test if variable is set
+  if [ "$sideFront" ]; then
+    # button is held down
+    xdotool click 4 # up
+    startAutoScroll=true
+  fi
+  
+  # +-------------+
+  # | AUTO SCROLL |
+  # +-------------+
+  if $startAutoScroll; then
+    # Loop used to "pause" the script before auto scoll.
+    # We check if the button is actually held down (to
+    # determine if this is a TRUE held down gesture all along)
+    for i in `seq 1 $autoScrollStep`; do
+	    side=`xinput --query-state $mouseId | grep "button\[[89]\]=down"`
+      if [ "$side" ]; then
+        echo "" > /dev/null
+      else
+        break
+      fi
+    done
+
+    sideBack=`xinput --query-state $mouseId | grep "button\[8\]=down"`
+    while [ "$sideBack" ]; do
+      # set "repeat" flag to control "delay"
+      xdotool click \
+        --repeat 1 \
+        --delay $autoScrollInterval \
+        5 # scroll down
+      
+      # re-evaluate loop condition
+      sideBack=`xinput --query-state $mouseId | grep "button\[8\]=down"`
+    done
+    
+    sideFront=`xinput --query-state $mouseId | grep "button\[9\]=down"`
+    while [ "$sideFront" ]; do
+      # set "repeat" flag to control "delay"
+      xdotool click \
+        --repeat 1 \
+        --delay $autoScrollInterval \
+        4 # scroll up
+      
+      # re-evaluate loop condition
+      sideFront=`xinput --query-state $mouseId | grep "button\[9\]=down"`
+    done
+  fi
+
+  # minimum sleep between each loop
+  # to lower CPU footprint
+  sleep 0.05
+done
+
+
+
